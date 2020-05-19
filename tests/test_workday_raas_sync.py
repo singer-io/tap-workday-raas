b39397b1ccd5920420f9d5c9ec64c371786af538
Kyle Allan <kyle@stitchdata.com>
2020-05-19 14:35:49 +0000

    Change report in integration test to one containing complex types

--- tests/test_workday_raas_sync.py
+++ tests/test_workday_raas_sync.py
@@ -37,7 +37,7 @@ class WorkdayRaasSync(unittest.TestCase):
         return {
             'start_date' : '2015-03-15 00:00:00',
             'username': os.getenv('TAP_WORKDAY_RAAS_USERNAME'),
-            'reports': '[{\"report_url\":\"https://i-0705abe4c72d24e6a.workdaysuv.com/ccx/service/customreport2/gms/lmcneil/stitch_test_report?End_Date=2020-02-19-08:00&Start_Date=2020-02-12-08:00\",\"report_name\":\"stitch_test_report\"}]'
+            'reports': '[{\"report_url\":\"https://wd2-impl-services1.workday.com/ccx/service/customreport2/talend_dpt1/lmcneil/Stitch_Test_Report?Roles%21WID=8d9435a66f9a431ab5377671801b9841\",\"report_name\":\"stitch_test_report\"}]'
         }
 
     def test_run(self):
