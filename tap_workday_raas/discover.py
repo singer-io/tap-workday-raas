bdb9e0bf8b33093270a94bd944bded5b0a1d4e58
Collin Simon <csimon@talend.com>
2020-05-26 18:09:00 +0000

    Fix typo and do the change to both exceptions

--- tap_workday_raas/discover.py
+++ tap_workday_raas/discover.py
@@ -14,7 +14,7 @@ def _element_to_schema(element):
 
     max_occurs = element.attrib.get("maxOccurs")
     if max_occurs not in (None, "unbounded"):
-        raise Exception("Found unexpected value for maxOccurs attribute.")
+        raise Exception("Found unexpected value for maxOccurs attribute: '{}'".format(max_occurs))
 
     is_list = max_occurs == 'unbounded'
 
@@ -85,7 +85,7 @@ def generate_schema_for_report(xsd):
 
             max_occurs = elem.attrib.get("maxOccurs")
             if max_occurs not in (None, "unbounded"):
-                raise Exception("Found unexpected value for maxOccurs attribute: '{}'".format(maxOccurs))
+                raise Exception("Found unexpected value for maxOccurs attribute: '{}'".format(max_occurs))
 
             is_list = max_occurs == 'unbounded'
 
