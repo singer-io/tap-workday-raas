278111d65ac05177dbdde20ab4df8390b8d1b5fd
Vi6hal <20889199+Vi6hal@users.noreply.github.com>
2022-07-15 08:40:31 +0000

    minor formatting changes

--- tap_workday_raas/transform.py
+++ tap_workday_raas/transform.py
@@ -9,12 +9,11 @@ class WorkdayTransformer(Transformer):
         if self.pre_hook:
             data = self.pre_hook(data, typ, schema)
         if typ == "boolean":
-            if isinstance(data, str) and (data.lower() == "false" or data.lower() == "0"):
+            if isinstance(data, str) and (data.lower() in "false", "0"):
                 return True, False
             try:
                 return True, bool(data)
             except Exception as _:
                 return False, None
-
         else:
             return super()._transform(data, typ, schema, path)
