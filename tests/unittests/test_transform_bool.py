c5f7bfced1a79f72d325eb5bd03af2f8f0f06594
Vi6hal <20889199+Vi6hal@users.noreply.github.com>
2022-07-19 08:42:42 +0000

    added testcase to verify bool transform

---
+++ tests/unittests/test_transform_bool.py
@@ -0,0 +1,17 @@
+import unittest
+from tap_workday_raas.transform import Transformer
+
+class TestBooleanTransform(unittest.TestCase):
+    """Test Case to Check Valid Boolean Transformation"""
+    transformer = getattr(Transformer(),"_transform")
+
+    def test_transform_eval_false(self,):
+        """Check if `0` & `False` is evaluated as False """
+        self.assertEqual(self.transformer("0","boolean",None,None),(True,False))
+        self.assertEqual(self.transformer("False","boolean",None,None),(True,False))
+
+
+    def test_transform_eval_true(self):
+        """Check if `1` & `True` is evaluated as True"""
+        self.assertEqual(self.transformer("1","boolean",None,None),(True,True))
+        self.assertEqual(self.transformer("True","boolean",None,None),(True,True))
