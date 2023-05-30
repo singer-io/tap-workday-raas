19df3ab6aa4ebeda13632948747946cc6c64b154
Andy Lu <andy@stitchdata.com>
2023-05-30 20:26:25 +0000

    Bump version, update changelog

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-workday-raas",
-    version="1.0.2",
+    version="1.0.3",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="http://singer.io",
