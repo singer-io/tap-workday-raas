682b8ac6575fd54bfcd81fc45a12169d3aa32072
jay-varicent <jama@varicent.com>
2023-10-05 17:04:23 -0400

    WP-17438 error message, traceback update

--- tap_workday_raas/__init__.py
+++ tap_workday_raas/__init__.py
@@ -65,19 +65,22 @@ def main():
         elif args.catalog or args.properties:
             do_sync(args.config, args.catalog, args.state)
     except SymonException as e:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
         error_info = {
-            'message': str(e),
+            'message': traceback.format_exception_only(exc_type, exc_value)[-1],
             'code': e.code,
-            'traceback': traceback.format_exc()
+            'traceback': "".join(traceback.format_tb(exc_traceback))
         }
 
         if e.details is not None:
             error_info['details'] = e.details
         raise
     except BaseException as e:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
         error_info = {
-            'message': str(e),
-            'traceback': traceback.format_exc()
+            'message': traceback.format_exception_only(exc_type, exc_value)[-1],
+            'code': e.code,
+            'traceback': "".join(traceback.format_tb(exc_traceback))
         }
         raise
     finally:
