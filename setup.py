9596e97baea0af1afc3a4f0b127e62758adfa909
Dylan Sprayberry <dylan.sprayberry@qlik.com>
2025-08-27 19:56:53 +0000

    See if bumping ijson fixes the python compatibility

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         "singer-python==5.13.2",
         "backoff==1.10.0",
         "requests==2.32.4",
-        "ijson==3.1.4",
+        "ijson==3.4.0",
     ],
     extras_require={"dev": ["ipdb", "pylint", "nose"]},
     entry_points="""
