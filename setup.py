0afd14cebc2801c12c19ee79971adbf588cb8969
mukeshbhatt18gl <mukesh.bhatt@qlik.com>
2026-07-30 12:29:22 +0530

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     install_requires=[
         "singer-python==6.8.0",
         "backoff==2.2.1",
-        "requests==2.33.0",
+        "requests==2.34.2",
         "ijson==3.4.0",
     ],
     extras_require={"dev": ["ipdb", "pylint", "nose"]},
